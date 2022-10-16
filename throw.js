686a94d6802a140e39852a46c0255b6b836767ca
realdino-jpj <78307958+realdino-jpj@users.noreply.github.com>
2022-10-16 17:51:03 +0530

    Add files via upload

---
+++ throw.js
@@ -0,0 +1,76 @@
+AFRAME.registerComponent("bowling-balls", {
+  init: function () {
+    this.throwBall();
+  },
+  throwBall: function () {
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "z") {
+        var  ball = document.createElement("a-entity");
+
+        ball.setAttribute("gltf-model", "./models/bowling_ball/scene.gltf");
+
+        ball.setAttribute("scale", { x: 3, y: 3,  z: 3});
+
+        var cam = document.querySelector("#camera");
+
+        pos = cam.getAttribute("position");
+
+        ball.setAttribute("position", {
+          x: pos.x,
+          y: pos.y-1.2,
+          z: pos.z,
+        });
+
+        var camera = document.querySelector("#camera").object3D;
+
+        //get the camera direction as Three.js Vector
+        var direction = new THREE.Vector3();
+        camera.getWorldDirection(direction);
+
+        //set the velocity and it's direction
+        ball.setAttribute("velocity", direction.multiplyScalar(-10));
+
+        var scene = document.querySelector("#scene");
+        
+        ball.setAttribute("dynamic-body",{
+         mass: 20
+        })
+        scene.appendChild(ball);
+
+        ball.addEventListener("collide",this.removeBullet)
+      }
+    });
+  },
+
+  removeBullet: function(e){
+    // original entity(ball) 
+
+    console.log(e.detail.target.el)
+
+    //original entity which ball touch
+    console.log(e.detail.body.el)
+    
+    var element = e.detail.target.el
+    var elementHit = e.detail.body.el
+
+    if(elementHit.id.includes("pin")){
+     elementHit.setAttribute("material",{
+      opacity: 0.6,
+     })
+
+   //impulsive and point vector
+   var impulse = new CANNON.Vec3(-2,2,1) 
+   var worldPoint = new CANNON.Vec3().copy(
+     elementHit.getAttribute("position")
+   
+   )
+   elementHit.body.applyImpulse(impulse,worldPoint)
+
+     element.removeEventListener("collide",this.shoot)
+     var scene = document.querySelector("#scene")
+     static-body  
+    }
+  }
+});
+
+
